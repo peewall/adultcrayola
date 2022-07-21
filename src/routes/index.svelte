4fe73f4aaa81c529ce9a2b43f4cd9bd53a6901df
Pete Wallace <ptw.wallace@gmail.com>
2022-07-21 14:51:37 -0400

    Show GraphQL data on index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,37 @@
+<script context="module">
+    import { client } from '$lib/graphql-client'
+    import { gql } from 'graphql-request'
+    
+    export const load = async () => {
+        const client = new GraphQLClient(import.meta.env.VITE_GRAPHQL_API)
+        const query = gql`
+        query GetProjects {
+            projects {
+            name
+            slug
+            description
+            demo
+            sourceCode
+            image {
+                url
+            }
+            }
+        }
+        `
+        const { projects } = await client.request(query)
+        return {
+        props: {
+            projects,
+        },
+        }
+    }
+   
+</script>
+<script>
+  export let projects
+</script>
+
+<pre>{JSON.stringify(projects, null, 2)}</pre>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
