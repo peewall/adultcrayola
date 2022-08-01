0c135038f9593e78bc0876ee8469b90974ab1b52
Pete Wallace <ptw.wallace@gmail.com>
2022-08-01 14:43:08 -0400

    x-overflow

--- src/routes/projects/index.svelte
+++ src/routes/projects/index.svelte
@@ -17,7 +17,7 @@
     let opaque = true
   </script>
   <svelte:window on:scroll={ _ => window.pageYOffset < 10 ? opaque = true : opaque = false }/>
-  <div class="container flex flex-col items-center min-w-full pt-12">
+  <div class="container flex flex-col items-center min-w-full pt-12 overflow-x-hidden">
       <div class="{opaque ? 'opacity-100': 'opacity-0'} fixed transition-opacity ease-in-out duration-666">
         <p class="font-main mx-auto text-3xl uppercase font-bold px-4 py-2 text-amber-50 bg-rose-500 text-center">designs + code.</p>
       </div>
